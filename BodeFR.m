592345c91268b9aae76d85baef9e9ab1caf40420
Max <medmaxim96@gmail.com>
2024-06-07 16:16:42 +0300

    Working script
    I don't like that it still doesn't calculate the input Amp and req from the given input signal recording.

--- BodeFR.m
+++ BodeFR.m
@@ -1,14 +1,16 @@
 %% Create the signal 
 clear all; close all; clc;
 
-ff_max = 6;
+ff_max = 500;
 Amp = 5;
 dt = 1/4000;
+repetetions = 10;
 uu = [];
 ffVec = [];
-for ff=1:2:ff_max
+ffToSample = 1:2:ff_max;
+for ff=ffToSample
     T = 1/ff;
-    tmp = Amp*sin(2*pi*ff*(0:dt:(5*T)));
+    tmp = Amp*sin(2*pi*ff*(0:dt:(repetetions*T)));
     ffVec = [ffVec zeros(1,100) ff*ones(1,size(tmp,2))];  
     uu = [uu zeros(1,100) tmp];    
 end
@@ -16,6 +18,7 @@ ffVec = [ffVec zeros(1,100)];
 uu = [uu zeros(1,100)];
 tt = dt * (0:size(uu,2)-1);
 
+figure(); title('Input');
 subplot(2,1,1);
 plot(tt, ffVec)
 subplot(2,1,2); 
@@ -23,21 +26,151 @@ plot(tt, uu)
 
 %% Create the plant + simulate the response + Add some noise
 s = tf('s');
-
-yy = lsim(((100*2*pi)^2/(s^2+0.707*(100*2*pi)*s+(100*2*pi)^2)), uu, tt);
+P = ((100*2*pi)^2/(s^2+0.707*(100*2*pi)*s+(100*2*pi)^2));
+yy = lsim(P, uu, tt);
 yy = awgn(yy,10,'measured'); % Add gaussian noise
 
+figure(); title('Output');
+subplot(2,1,1);
+plot(tt, ffVec)
+subplot(2,1,2); 
+plot(tt, yy)
+
 % Find the indices where theres a recording
-indices = find(abs(diff(ffVec))>0);
-indices = reshape([indices size(ffVec,2)],2,[])';
+indexMatrix = extractSinusoidIndices(uu);
+indexMatrix(:,1) = indexMatrix(:,1)-1; %Cause we start with zero
+
+% Extract the signals to cells
+yys = {}; tts = {}; kk = 1;
+for ii = indexMatrix'
+    yys{kk} = yy(ii(1):ii(2));
+    uus{kk} = uu(ii(1):ii(2));
+    tts{kk} = tt(ii(1):ii(2))-tt(ii(1));
+    kk = kk+1;
+end
+
+%% Estimate the transfer function and compare to GT
+ffs = []; gains = []; phases = []; kk = 1;
+repetitons = 10;
+
+for kk = 1:size(yys,2)
+    % Need to know the tested freq and amplitude (can estimate from input
+    % probably)
+    tts_filtered = tts{kk};
+    ff = ffToSample(kk);
+    
+    % Clean the signals (no phase filter filtfilt)
+    wn = ff*2*pi;
+    BF = s*wn^2/(s+wn)^2;
+    BF = (1/abs(evalfr(BF,1i*wn)))*BF;
+    BF_dig = c2d(BF, dt);    
+    yy_filtered = filtfilt(BF_dig.Numerator{:}, BF_dig.Denominator{:}, yys{kk});  
+    
+    % Estimate the output sinus
+    [gain, phase] = FitResponse(tts_filtered', yy_filtered, ff);
+    ffs = [ffs ff];
+    gains = [gains gain/Amp]; 
+    phases = [phases phase];
+end
+phases = phaseFixer(phases);
+%% Compare the results
+[gain_GT, phase_GT, wout, ~, ~] = bode(P, ffs*2*pi);
 
 % Filter using the frequency we know we recorded (Using filtfilt to not affect the phase)
-wn = 1*2*pi;
-BF = s*wn^2/(s+wn)^2;
-BF_dig = c2d(BF, dt);
-yy_filt = filtfilt(BF_dig.Numerator{:}, BF_dig.Denominator{:}, yy);
-
+figure()
 subplot(2,1,1);
-plot(tt, yy);
+semilogx(ffs, mag2db(squeeze(gain_GT)), '-o');hold on;
+semilogx(ffs, mag2db(gains), '-o');hold off;
 subplot(2,1,2); 
-plot(tt, yy_filt)
+semilogx(ffs, squeeze(phase_GT), '-o');hold on;
+semilogx(ffs, phases*180/pi, '-o');hold off;
+legend('GT', 'Measured');
+
+%% Functions
+function [gain, phase] = FitResponse(tts_filtered, yy_filtered, ff)
+    %% Fit: 'FitResponse'.
+    [xData, yData] = prepareCurveData( tts_filtered, yy_filtered );
+
+    % Set up fittype and options.
+    ft = fittype( 'a*sin(2*pi*'+string(ff)+'*x+b)', 'independent', 'x', 'dependent', 'y' );
+    opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
+    opts.Display = 'Off';
+    opts.Lower = [0 -Inf];
+    opts.Upper = [Inf 0];
+    opts.StartPoint = [0.620968655826104 0.690192179535585];
+
+    % Fit model to data.
+    [fitresult, ~] = fit( xData, yData, ft, opts );
+    gain = fitresult.a;
+    phase = fitresult.b;
+end
+
+function indexMatrix = extractSinusoidIndices(paddedArray)
+    % Initialize variables
+    indices = []; % Matrix to hold start and end indices of sinusoidal segments
+    currentStartIndex = -1; % Start index of the current sinusoidal segment
+    zeroPaddingCount = 0; % Counter for zeros
+    
+    % Iterate through the array
+    for i = 1:length(paddedArray)
+        if paddedArray(i) ~= 0
+            % If a non-zero element is found, reset zero padding count
+            zeroPaddingCount = 0;
+            % If it's the start of a new sinusoidal segment, set the start index
+            if currentStartIndex == -1
+                currentStartIndex = i;
+            end
+        else
+            % If a zero element is found, increment the zero padding count
+            zeroPaddingCount = zeroPaddingCount + 1;
+            % If the zero padding is more than 10 and a sinusoidal segment was ongoing
+            if zeroPaddingCount > 10 && currentStartIndex ~= -1
+                % Store the start and end indices of the current sinusoidal segment
+                indices = [indices; currentStartIndex, i - zeroPaddingCount]; %#ok<*AGROW>
+                % Reset the start index
+                currentStartIndex = -1;
+            end
+        end
+    end
+    
+    % If the array ends with a non-zero segment, store its indices
+    if currentStartIndex ~= -1
+        indices = [indices; currentStartIndex, length(paddedArray)];
+    end
+    
+    % Convert the indices matrix to the desired format
+    indexMatrix = indices;
+end
+
+function o_phases = phaseFixer(i_phases)
+    o_phases = [];
+    for kk=1:size(i_phases,2)
+        if kk==1
+            o_phases(kk) = i_phases(kk);
+            continue 
+        else
+            phase = i_phases(kk);
+            % Decide where the current angle is relative to its predecessor
+            if phase<o_phases(kk-1)
+                dir = 1;
+            else
+                dir = -1;
+            end
+            intiDist = abs(phase-o_phases(kk-1));
+            while true
+                if (phase+dir*pi)>0 % Dont want to go above zero
+                    break
+                end
+                % Depoending on the direction we want to add incremetns
+                % this is done while were getting close to the value
+                if abs(phase+dir*pi-o_phases(kk-1))<intiDist
+                    phase = phase+dir*pi;
+                    intiDist = abs(phase-o_phases(kk-1));
+                else 
+                    break
+                end    
+            end
+            o_phases(kk) = phase;
+        end
+    end
+end
